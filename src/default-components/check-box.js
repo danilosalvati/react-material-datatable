bf131f30e668b1de977935a073ad9a25bd413a7b
Danilo Salvati <sadan91@gmail.com>
2016-08-19 01:00:29 +0200

    Now the check-box component is stateless

--- src/default-components/check-box.js
+++ src/default-components/check-box.js
@@ -4,48 +4,37 @@ const UNDETERMINATED_MODE = 'undeterminated';
 const SELECTED_MODE = 'selected';
 const UNSELECTED_MODE = 'unselected';
 
-export default class CheckBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.changeMode = this.changeMode.bind(this);
+export default function CheckBox({checkBoxMode, isHeader, onChangeFunction}) {
+
+  let fillOpacity = '0.87';
+  if (isHeader) {
+    fillOpacity = '0.54';
   }
 
-  changeMode() {
-    // Now I can execute the listener defined by the user
-    this.props.onChangeFunction();
-  }
-
-  render() {
-
-    let fillOpacity = '0.87';
-    if (this.props.isHeader) {
-      fillOpacity = '0.54';
-    }
-
-    let path;
-    switch (this.props.checkBoxMode) {
-      case UNDETERMINATED_MODE:
-        path = (<path fill='#2196F3' fillOpacity={fillOpacity}
-                      d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z"/>);
-        break;
-      case SELECTED_MODE:
-        path = (<path fill='#2196F3' fillOpacity={fillOpacity}
-                      d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9
+  let path;
+  switch (checkBoxMode) {
+    case UNDETERMINATED_MODE:
+      path = (<path fill='#2196F3' fillOpacity={fillOpacity}
+                    d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z"/>);
+      break;
+    case SELECTED_MODE:
+      path = (<path fill='#2196F3' fillOpacity={fillOpacity}
+                    d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9
         14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>);
-        break;
-      case UNSELECTED_MODE:
-      default:
-        path = (<path fillOpacity={fillOpacity}
-                      d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>);
-    }
-
-    return (
-      <div onClick={this.changeMode}>
-        <svg style={{paddingLeft:'24px', paddingRight:'24px'}} width='24px' height='24px'>
-          {path}
-        </svg>
-      </div>);
+      break;
+    case UNSELECTED_MODE:
+    default:
+      path = (<path fillOpacity={fillOpacity}
+                    d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>);
   }
+
+  return (
+    <div onClick={onChangeFunction}>
+      <svg style={{paddingLeft:'24px', paddingRight:'24px'}} width='24px' height='24px'>
+        {path}
+      </svg>
+    </div>
+  );
 }
 
 CheckBox.propTypes = {
@@ -53,7 +42,6 @@ CheckBox.propTypes = {
   isHeader: React.PropTypes.bool.isRequired,
   onChangeFunction: React.PropTypes.func
 };
-
 
 CheckBox.defaultProps = {
   onChangeFunction: () => {
