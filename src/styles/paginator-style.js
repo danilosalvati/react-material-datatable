2ebf44e02afd932a89ed28f137fbd8142c69e7e5
Danilo Salvati <sadan91@gmail.com>
2016-08-20 01:50:33 +0200

    Added pagination bar (only styles)

---
+++ src/styles/paginator-style.js
@@ -0,0 +1,26 @@
+export default {
+  paginator: {
+    border: '1px solid rgba(0,0,0,0.10)',
+    height: '56px',
+    fontFamily: 'Roboto, sans-serif',
+    fontStyle: 'normal',
+    fontSize: '12px',
+    color:'rgba(0,0,0,0.54)',
+  },
+  paginatorContent: {
+    float: 'right',
+    marginRight: '14px',
+    marginTop: '16px'
+  },
+  selectStyle: {
+    backgroundColor: '#fff',
+    marginLeft: '20px',
+    marginRight: '32px',
+    border:'none',
+    fontFamily: 'Roboto, sans-serif',
+    fontStyle: 'normal',
+    fontSize: '12px',
+    color:'rgba(0,0,0,0.54)',
+
+  }
+}
