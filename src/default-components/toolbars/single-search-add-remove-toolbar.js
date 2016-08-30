b24f50f4638107725901585678a8a6fb58d8d4af
Danilo Salvati <sadan91@gmail.com>
2016-08-30 23:23:39 +0200

    Added state to the toolbar

--- src/default-components/toolbars/single-search-add-remove-toolbar.js
+++ src/default-components/toolbars/single-search-add-remove-toolbar.js
@@ -5,26 +5,39 @@ import SearchButton from './search-button';
 import AddButton from './add-button';
 import DeleteButton from './delete-button'
 
-export default function SingleSearchAndAddToolbar({title}) {
 
-  return (
-    <div style={headerStyle.toolbarStyle}>
-      <div style={headerStyle.contentStyle}>
-        <label style={{verticalAlign:'middle'}}>{title}</label>
-        <div style={headerStyle.buttonsBar}>
-          <div style={{paddingLeft:'24px'}}>
-            <SearchButton/>
-          </div>
-          <div style={{paddingLeft:'24px'}}>
-            <DeleteButton isEnabled={false}/>
-          </div>
-          <div style={{paddingLeft:'24px'}}>
-            <AddButton/>
+export default class SingleSearchAndAddToolbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searching: false};
+  }
+
+  render() {
+
+    let contentBar = ( <label style={{verticalAlign:'middle'}}>{this.props.title}</label>);
+    if (this.state.searching) {
+      contentBar = (<input type="text" placeholder='Search...'></input>);
+    }
+
+    return (
+      <div style={headerStyle.toolbarStyle}>
+        <div style={headerStyle.contentStyle}>
+          {contentBar}
+          <div style={headerStyle.buttonsBar}>
+            <div style={{paddingLeft:'24px'}}>
+              <SearchButton/>
+            </div>
+            <div style={{paddingLeft:'24px'}}>
+              <DeleteButton isEnabled={false}/>
+            </div>
+            <div style={{paddingLeft:'24px'}}>
+              <AddButton/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 SingleSearchAndAddToolbar.propTypes = {
